18185acd4755839a4f00c9a948adda8de0b14c61
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-03-20 14:48:39 +0200

    add random color

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-const a = 1;
-console.log(a);
+// 'use strict';
 
-const b = 2;
-console.log(b);
+// a = 1;
+// console.log(a);
+
+// b = 2;
+// console.log(b);
+
+import SimpleLightbox from 'simplelightbox';
+
+const d = document.querySelector('body');
+
+const randomColor = require('randomcolor');
+const color = randomColor();
+d.style.backgroundColor = randomColor();
+
+console.log(color);
