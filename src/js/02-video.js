df5bcea2ba0cd3ab06fa5bab0fa17fa5849c6bc2
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-03-21 19:57:48 +0200

    homework8 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,39 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+// import * as _throttle from 'lodash.throttle';
+
 const iframe = document.getElementById('vimeo-player');
 // const iframe = document.querySelector('#vimeo-player');
 
-const player = new Player(iframe, {
-  id: 'vimeo-player',
-  src: 'https://player.vimeo.com/video/236203659',
-  width: '640',
-  height: '360',
-  frameborder: '0',
-  allowfullscreen: true,
-  allow: 'autoplay; encrypted-media',
-});
+// const player = new Player(iframe, {
+//   id: 'vimeo-player',
+//   src: 'https://player.vimeo.com/video/236203659',
+//   width: '640',
+//   height: '360',
+//   frameborder: '0',
+//   allowfullscreen: true,
+//   allow: 'autoplay; encrypted-media',
+// });
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+const player = new Player(iframe);
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+player.on(
+  'timeupdate',
+  throttle(function (time) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+    console.log(time);
+    // data is an object containing properties specific to that event
+  }, 1000)
+);
+
+const savedSettings = localStorage.getItem('videoplayer-current-time');
+const parsedSettings = JSON.parse(savedSettings);
+
+// if (parsedSettings) parsedSettings.seconds = 0;
+
+if (parsedSettings) player.setCurrentTime(parsedSettings.seconds);
+
+// player.setCurrentTime(parsedSettings.seconds).then(function (e) {
+//   console.log(e);
+//   // seconds = the actual time that the player seeked to
+// });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const formEd = document.querySelector('.feedback-form');
+const inputE = document.querySelector('input');
+const inputM = document.querySelector('textarea');
+
+let tempVault = {
+  email: '',
+  message: '',
+};
+
+if (localStorage.getItem('feedback-form-state')) {
+  tempVault = JSON.parse(localStorage.getItem('feedback-form-state'));
+  inputE.value = tempVault.email;
+  inputM.value = tempVault.message;
+}
+
+formEd.addEventListener(
+  'input',
+  throttle(event => {
+    tempVault[event.target.name] = event.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(tempVault));
+  }),
+  500
+);
+
+formEd.addEventListener('submit', event => {
+  event.preventDefault();
+  formEd.reset();
+  console.log(tempVault);
+
+  localStorage.removeItem('feedback-form-state');
+
+  tempVault = {
+    email: '',
+    message: '',
+  };
+});
