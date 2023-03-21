b2ae0cb3d9b23e58310408d7e81c1b099517b7a3
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-03-21 16:43:25 +0200

    setting up the player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+const iframe = document.getElementById('vimeo-player');
+// const iframe = document.querySelector('#vimeo-player');
 
+const player = new Player(iframe, {
+  id: 'vimeo-player',
+  src: 'https://player.vimeo.com/video/236203659',
+  width: '640',
+  height: '360',
+  frameborder: '0',
+  allowfullscreen: true,
+  allow: 'autoplay; encrypted-media',
+});
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
